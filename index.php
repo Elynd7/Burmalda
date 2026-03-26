a67399b0b5e410b768e2af48c901a82ce73a76f9
Edos <rich@gmail.com>
2026-03-26 13:01:56 +0300

    1ч 8з

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
-    unlink("folder/world.txt");
+    if (file_exists("folder/world.txt")) {
+    echo "Файл world.txt существует.<br>";
+    } else {
+        echo "Файл world.txt НЕ существует.<br>";
+    }
+    if (file_exists("folder/mir.txt")) {
+    echo "   Файл mir.txt существует.<br>";
+    } else {
+        echo "   Файл mir.txt НЕ существует.<br>";
+    }
 ?>
+
