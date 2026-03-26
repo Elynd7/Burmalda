59fb43601688d8d4178613b06b14e9d73ea484f6
Edos <rich@gmail.com>
2026-03-26 13:02:35 +0300

    2ч 1з

--- index.php
+++ index.php
@@ -1,13 +1,4 @@
 <?php
-    if (file_exists("folder/world.txt")) {
-    echo "Файл world.txt существует.<br>";
-    } else {
-        echo "Файл world.txt НЕ существует.<br>";
-    }
-    if (file_exists("folder/mir.txt")) {
-    echo "   Файл mir.txt существует.<br>";
-    } else {
-        echo "   Файл mir.txt НЕ существует.<br>";
-    }
+    mkdir("test");
 ?>
 
