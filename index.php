8a2fe66193312418d438f0fc4680958a04d50696
Edos <rich@gmail.com>
2026-03-26 12:59:34 +0300

    1ч 6з исправленно отображение мг и гб

--- index.php
+++ index.php
@@ -4,6 +4,6 @@
     $gb = $size / 1024 / 1024 / 1024;
     echo "Размер файла world.txt:<br>";
     echo "Байт: " . $size . "<br>";
-    echo "Мегабайт: " . $mb . "<br>";
-    echo "Гигабайт: " . $gb . "<br>";
+    echo "   Мегабайт: " . number_format($mb, 10, '.', '') . "<br>"; 
+    echo "   Гигабайт: " . number_format($gb, 10, '.', '') . "<br>";
 ?>
