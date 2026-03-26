d9071070aacd314c6e3ed8f9607c9d346ac41116
Edos <rich@gmail.com>
2026-03-26 13:57:25 +0300

    2ч 8з

--- index.php
+++ index.php
@@ -1,23 +1,5 @@
 <?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $date1 = $_POST['date1'] ?? '';
-    $date2 = $_POST['date2'] ?? '';
-    
-    $ts1 = strtotime($date1);
-    $ts2 = strtotime($date2);
-    
-    if ($ts1 > $ts2) {
-        echo "Более поздняя дата: $date1";
-    } elseif ($ts2 > $ts1) {
-        echo "Более поздняя дата: $date2";
-    } else {
-        echo "Даты равны";
-    }
-}
+$inputDate = '2025-12-31';
+$timestamp = strtotime($inputDate);
+echo date('d-m-Y', $timestamp);
 ?>
-
-<form method="POST">
-    Дата 1 ГГГГ-ММ-ДД: <input type="date" name="date1" required><br>
-    Дата 2 ГГГГ-ММ-ДД: <input type="date" name="date2" required><br>
-    <input type="submit" value="Сравнить">
-</form>
