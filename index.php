514aac10796dd0db0d2a613c757e1957e3b122aa
Edos <rich@gmail.com>
2026-03-26 11:57:34 +0300

    1ч 6з

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php
-    copy("folder/mir.txt", "folder/world.txt");
+    $size = filesize("folder/world.txt");
+    $mb = $size / 1024 / 1024;
+    $gb = $size / 1024 / 1024 / 1024;
+    echo "Размер файла world.txt:<br>";
+    echo "Байт: " . $size . "<br>";
+    echo "Мегабайт: " . $mb . "<br>";
+    echo "Гигабайт: " . $gb . "<br>";
 ?>
