76e8461c63179b78582c86f56f3ffe8df004b0a3
Edos <rich@gmail.com>
2026-03-26 13:58:01 +0300

    2ч 9з

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
-$inputDate = '2025-12-31';
-$timestamp = strtotime($inputDate);
-echo date('d-m-Y', $timestamp);
+$date = date_create('2000-02-03');
+
+date_modify($date, '+1 year +1 month +5 days');
+echo "После прибавления: " . date_format($date, 'd.m.Y') . "<br>";
+
+date_modify($date, '-3 days');
+echo "После вычитания 3 дней: " . date_format($date, 'd.m.Y'); 
 ?>
