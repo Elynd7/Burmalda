088b730169cc9e315e68862efde1ea511cb0785c
Edos <rich@gmail.com>
2026-03-26 13:40:14 +0300

    2ч 1з

--- index.php
+++ index.php
@@ -1,13 +1,3 @@
 <?php
-$countries = ['Spain' => 'Madrid', 'Russia' => 'Moscow'];
-$key = 'Germany';
-
-try {
-    if (!array_key_exists($key, $countries)) {
-        throw new Exception("Ключ '$key' не существует в массиве");
-    }
-    echo $countries[$key];
-} catch (Exception $ex) {
-    echo $ex->getMessage();
-}
+echo mktime(10, 25, 0, 3, 15, 2025);
 ?>
