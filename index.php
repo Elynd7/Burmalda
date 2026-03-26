699a3de6aa63c500c09a45aac8b5513afbf0189b
Edos <rich@gmail.com>
2026-03-26 13:00:45 +0300

    1ч 7з

--- index.php
+++ index.php
@@ -1,9 +1,3 @@
 <?php
-    $size = filesize("folder/world.txt");
-    $mb = $size / 1024 / 1024;
-    $gb = $size / 1024 / 1024 / 1024;
-    echo "Размер файла world.txt:<br>";
-    echo "Байт: " . $size . "<br>";
-    echo "   Мегабайт: " . number_format($mb, 10, '.', '') . "<br>"; 
-    echo "   Гигабайт: " . number_format($gb, 10, '.', '') . "<br>";
+    unlink("folder/world.txt");
 ?>
