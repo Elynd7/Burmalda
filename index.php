59da6fc9cbc8380c8af7376046edadb56e5c1cf1
Edos <rich@gmail.com>
2026-04-08 11:13:02 +0300

    1-3 задание

--- index.php
+++ index.php
@@ -1,25 +1,20 @@
 <?php
-
 class Worker {
     public $name;
     public $age;
     public $salary;
-
-    public function getName() { return $this->name; }
-    public function getAge() { return $this->age; }
-    public function getSalary() { return $this->salary; }
 }
 
 $worker1 = new Worker();
-$worker1->name = "Кулаков Денис"; $worker1->age = 18; $worker1->salary = 3000;
+$worker1->name = "Иван Иванов";
+$worker1->age = 25;
+$worker1->salary = 50000;
 
 $worker2 = new Worker();
-$worker2->name = "Костылев Эдуард"; $worker2->age = 18; $worker2->salary = 1000000;
+$worker2->name = "Петр Петров";
+$worker2->age = 30;
+$worker2->salary = 60000;
 
-
-$totalSalary = $worker1->getSalary() + $worker2->getSalary();
-echo "Сумма зарплат: {$totalSalary} руб.<br>";
-echo "Сумма возрастов: " . ($worker1->age + $worker2->age) . " лет";
-
-
+echo "Сумма зарплат: " . ($worker1->salary + $worker2->salary) . " руб.\n";
+echo "Сумма возрастов: " . ($worker1->age + $worker2->age) . " лет\n";
 ?>
